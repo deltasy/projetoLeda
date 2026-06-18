7b57fa93758a0e1d9b9cf1d7c7c121d1cc4e667a
Resloko <duckedevel@gmail.com>
2026-06-18 13:26:31 -0300

    Busca binária

--- src/main/java/projetoleda/com/busca/BuscaBinariaRecursiva.java
+++ src/main/java/projetoleda/com/busca/BuscaBinariaRecursiva.java
@@ -1,4 +1,31 @@
 package projetoleda.com.busca;
 
 public class BuscaBinariaRecursiva {
+    public static <T extends Comparable<T>> int buscar(T[] lista, T alvo) {
+        return buscar(lista, alvo, 0, lista.length - 1);
+    }
+
+    private static <T extends Comparable<T>> int buscar(
+            T[] lista,
+            T alvo,
+            int inicio,
+            int fim
+    ) {
+        if (inicio > fim) {
+            return -1;
+        }
+
+        int meio = inicio + (fim - inicio) / 2;
+        int comparacao = alvo.compareTo(lista[meio]);
+
+        if (comparacao == 0) {
+            return meio;
+        }
+
+        if (comparacao < 0) {
+            return buscar(lista, alvo, inicio, meio - 1);
+        }
+
+        return buscar(lista, alvo, meio + 1, fim);
+    }
 }
